3107e170dc16dbfa3bf1133323ffe0bb73571214
nizar zahmouli <nizarzahmouli351@gmail.com>
2023-04-10 03:14:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import ProfilePhoto from './components/ProfilePhoto'
+import FullName from './components/FullName'
 import './App.css';
+import Address from './components/Address';
 
 function App() {
   return (
     <div className="App">
+      <h1>My Profile</h1>
+      <ProfilePhoto/>
+      <FullName/>
+      <Address/>
+      
       
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Address.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Address() {
+  return (
+    <div>
+      <p>Tunisia , Sfax ,Skhira 3050</p>
+    </div>
+  );
+}
+
+export default Address;
